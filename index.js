71007159172661625537b9bfb862ac8b33c26987
johan guxman <johan.dev19@gmail.com>
2020-01-25 18:07:19 -0400

    add the shuffleDominos function and distribute dominos to all players

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const numberOfPlayers = 4;
 
 let startDominoNumber = 6;
 let currenDominoNumber = 0;
+let shuffledDominos;
+
+function getRandomInt(array) {
+  return Math.floor(Math.random() * array.length);
+}
 
 while (dominos.length < maxNumberOfDominos) {
   if (currenDominoNumber > startDominoNumber) {
@@ -16,4 +21,33 @@ while (dominos.length < maxNumberOfDominos) {
   currenDominoNumber++;
 }
 
-console.log(dominos)
+function shuffleDominos(array) {
+  const temp = [];
+  let len = array.length;
+  while (len) {
+    temp.push(array[getRandomInt(dominos)]);
+    len--;
+  }
+  return temp;
+}
+
+function distributeDominos(dominos) {
+  let currentPlayer = 0;
+  const dominosPerPlayer = dominos.length / numberOfPlayers;
+
+  while(currentPlayer < numberOfPlayers) {
+    players.push({
+      playerId: currentPlayer,
+      playerName: `player ${currentPlayer}`,
+      dominos: dominos.splice(0,dominosPerPlayer)
+    })
+
+    currentPlayer++
+  }
+
+  return players;
+}
+
+shuffleDominos = shuffleDominos(dominos);
+distributeDominos(shuffleDominos);
+console.table(players)
